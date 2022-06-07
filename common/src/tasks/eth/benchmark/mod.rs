bb4bb3e41bfd7fce9b9ffb4826659b2a6de94a66
anhhuy <anhhuy0501@gmail.com>
2022-06-07 10:42:16 +0700

    revise task

--- common/src/tasks/eth/benchmark/mod.rs
+++ common/src/tasks/eth/benchmark/mod.rs
@@ -1,2 +1,29 @@
+pub mod executor;
 pub mod generator;
+use anyhow::{anyhow, Error};
 pub use generator::*;
+use thiserror::Error;
+
+#[derive(Error, Debug, Clone)]
+pub enum CallBenchmarkError {
+    #[error("send error")]
+    SendError(String),
+    #[error("get body error")]
+    GetBodyError(String),
+}
+
+impl CallBenchmarkError {
+    pub fn get_message(&self) -> String {
+        match self {
+            CallBenchmarkError::SendError(message) | CallBenchmarkError::GetBodyError(message) => {
+                message.to_string()
+            }
+        }
+    }
+    pub fn get_code(&self) -> u32 {
+        match self {
+            CallBenchmarkError::SendError(_) => 1u32,
+            CallBenchmarkError::GetBodyError(_) => 2u32,
+        }
+    }
+}
