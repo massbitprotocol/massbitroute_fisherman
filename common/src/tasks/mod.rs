ca96eb9a0f56be3b1ea8ecbde7bef85435487809
anhhuy <anhhuy0501@gmail.com>
2022-06-07 09:37:01 +0700

    update load config

--- common/src/tasks/mod.rs
+++ common/src/tasks/mod.rs
@@ -6,26 +6,29 @@ pub mod ping;
 
 use crate::job_manage::JobRole;
 pub use executor::get_eth_executors;
-use log::error;
+use log::{error, info};
 use serde::de::DeserializeOwned;
 use serde_json::Value;
 use std::collections::HashMap;
+use std::fmt::Debug;
 use std::iter::Map;
 
 const DEFAULT_KEY: &str = "default";
 const VERIFICATION_KEY: &str = "verification";
 const FISHERMAN_KEY: &str = "fisherman";
-pub trait LoadConfig<T: DeserializeOwned + Default> {
+pub trait LoadConfig<T: DeserializeOwned + Default + Debug> {
     fn load_config(path: &str, role: &JobRole) -> T {
         let json = std::fs::read_to_string(path).unwrap();
         let configs: Value = serde_json::from_str(&*json).unwrap();
         let mut default_config: T =
             serde_json::from_value(configs.get(DEFAULT_KEY).unwrap().clone()).unwrap();
-        let mut modify_config: Result<T, _> =
-            serde_json::from_value(configs.get(role.to_string()).unwrap().clone());
-        if let Ok(modify_config) = modify_config {
-            default_config = modify_config;
+        if let Some(modify_config) = configs.get(role.to_string()) {
+            let mut modify_config: Result<T, _> = serde_json::from_value(modify_config.clone());
+            if let Ok(modify_config) = modify_config {
+                default_config = modify_config;
+            }
         }
+        info!("Loaded config: {:#?} for role: {:?}", default_config, role);
         default_config
     }
 }
