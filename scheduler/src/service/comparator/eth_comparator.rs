15cbaa952d7e163de44a51c551ab4ebcc8799980
anhhuy <anhhuy0501@gmail.com>
2022-07-01 13:32:33 +0700

    Auto stash before checking out "origin/master"

--- scheduler/src/service/comparator/eth_comparator.rs
+++ scheduler/src/service/comparator/eth_comparator.rs
@@ -1,4 +1,5 @@
 use crate::service::comparator::Comparator;
+use anyhow::{anyhow, Error};
 use common::tasks::http_request::HttpResponseValues;
 use common::util::get_current_time;
 use log::info;
@@ -8,12 +9,12 @@ use serde::{Deserialize, Serialize};
 pub struct LatestBlockEthComparator {}
 
 impl LatestBlockEthComparator {
-    pub fn get_block_time(values: &HttpResponseValues) -> i64 {
+    pub fn get_block_time(values: &HttpResponseValues) -> Result<i64, Error> {
         let timestamp = values
             .get("timestamp")
             .and_then(|val| val.as_str())
             .and_then(|str| i64::from_str_radix(str, 16).ok())
-            .unwrap_or_default();
+            .ok_or(anyhow!("Error parse timestamp"));
         timestamp
     }
 }
@@ -21,9 +22,17 @@ impl Comparator for LatestBlockEthComparator {
     fn get_latest_value(&self, value: &HttpResponseValues) -> Option<i64> {
         self.get_number_value(value, "timestamp")
     }
-    fn compare(&self, value1: &HttpResponseValues, value2: &HttpResponseValues) -> i64 {
-        let block_time1 = Self::get_block_time(value1);
-        let block_time2 = Self::get_block_time(value2);
-        block_time1 - block_time2
+    fn compare(
+        &self,
+        value1: &HttpResponseValues,
+        value2: &HttpResponseValues,
+    ) -> Result<i64, Error> {
+        if let (Ok(block_time1), Ok(block_time2)) =
+            (Self::get_block_time(value1), Self::get_block_time(value2))
+        {
+            Ok(block_time1 - block_time2)
+        } else {
+            Err(anyhow!("Error parse timestamp"))
+        }
     }
 }
