028990a6ac0adb295a81944390d67e817b6b1664
hoanito <hhoang219@gmail.com>
2022-07-15 11:03:57 +0700

    update docker

---
+++ scripts/run_worker_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export ROOT=/massbit/massbitroute/app/src/sites/services/gateway/
+export DOMAIN=massbitroute.net
+export RUST_LOG=info
+export SCHEDULER_ENDPOINT=https://scheduler.fisherman.$DOMAIN
+export WORKER_ID=aaaaaaaaaaaaaaaaaa
+export WORKER_IP=172.24.24.204
+export ZONE=AS
+export WORKER_ENDPOINT=https://$WORKER_IP/__worker
+export WORKER_SERVICE_ENDPOINT=0.0.0.0:4040
+export BENCHMARK_WRK_PATH=benchmark
+
+./fisherman
