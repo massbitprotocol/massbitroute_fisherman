79337bcb921a064dcefeb10b06714760d8a80c18
hoanito <hhoang219@gmail.com>
2022-07-15 11:56:39 +0700

    update docker

--- scripts/run_worker_docker.sh
+++ scripts/run_worker_docker.sh
@@ -7,7 +7,7 @@ export SCHEDULER_ENDPOINT=http://scheduler.fisherman.$DOMAIN
 export WORKER_ID=default_worker_id
 export WORKER_IP=172.24.24.204
 export ZONE=AS
-export WORKER_ENDPOINT=http://$WORKER_IP/__worker
+export WORKER_ENDPOINT=http://$WORKER_IP:4040
 export WORKER_SERVICE_ENDPOINT=0.0.0.0:4040
 export BENCHMARK_WRK_PATH=benchmark
 
