e3ba7f9f23d259b3e58235afbc841c99ad240027
anhhuy <anhhuy0501@gmail.com>
2022-07-26 15:21:57 +0700

    Update scheduler deploy script

--- scripts/deploy_scheduler.sh
+++ scripts/deploy_scheduler.sh
@@ -4,5 +4,6 @@ strip ../target/release/scheduler
 rsync -avz ../target/release/scheduler "scheduler:~/"
 rsync -avz ../scheduler/configs "scheduler:~/"
 rsync -avz ./run_scheduler.sh "scheduler:~/run.sh"
+rsync -avz ../scheduler/.env_deploy "scheduler:~/.env"
 
-ssh "scheduler" < update_bin_and_restart_service.sh
+ssh "scheduler" < update_bin_and_restart_scheduler.sh
