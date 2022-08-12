9a23c43b077df42c0722794ce9a450971f342ca2
hoanito <hhoang219@gmail.com>
2022-08-12 12:08:45 +0700

    update config

--- scripts/test/1_pre_config.sh
+++ scripts/test/1_pre_config.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+if [ -z "$1" ]
+then
+    exit 1
+fi
+NETWORK_NUMBER=$1
 
-docker exec mbr_api bash -c '/massbit/massbitroute/app/src/sites/services/api/cmd_server start nginx'
-docker exec mbr_portal_api bash -c 'cd /app; npm run dbm:init'
-docker exec mbr_db bash -c 'bash /docker-entrypoint-initdb.d/3_init_user.sh'
+docker exec mbr_api_$NETWORK_NUMBER bash -c '/massbit/massbitroute/app/src/sites/services/api/cmd_server start nginx'
+docker exec mbr_portal_api_$NETWORK_NUMBER bash -c 'cd /app; npm run dbm:init'
+docker exec mbr_db_$NETWORK_NUMBER bash -c 'bash /docker-entrypoint-initdb.d/3_init_user.sh'
