9bfd7ee1955369587ce2b50b195969ee7251b5cb
Viet Tai <vuviettai@gmail.com>
2022-09-09 17:07:24 +0700

    Add docker build

---
+++ dockerize/docker_build.sh
@@ -0,0 +1,10 @@
+#docker build -f RustBuilderDockerfile -t rustbuilder:1.61.0 bash .
+docker run -it -name rustbuilder -v $(pwd)/..:/fisherman rustbuilder:1.61.0 bash -c 'cd /fisherman && cargo build --release'
+docker exec -it rustbuilder bash -c 'cd /fisherman && cargo build --release'
+
+cp -r ../scripts/benchmark .
+cp ../target/release/scheduler .
+cp ../target/release/fisherman .
+cp ../scripts/build_docker/services .
+docker build -f RuntimeDockerfile -t massbit/massbitroute_fisherman:${1-v0.1.0-dev} .
+rm -rf ./benchmark ./scheduler ./fisherman ./services
