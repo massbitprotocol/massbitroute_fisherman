3760727a1a97cc84f1d53b0ca41665eb96f0d3c0
anhhuy <anhhuy0501@gmail.com>
2022-09-23 10:45:05 +0700

    add log config

--- logger/src/core_logger.rs
+++ logger/src/core_logger.rs
@@ -6,14 +6,20 @@
  **/
 use crate::helper::{log_to_console, log_to_file, message};
 use lazy_static::lazy_static;
+use log::LevelFilter;
+use log4rs::config::Config;
+use log4rs::Error;
 use std::env;
+use std::str::FromStr;
 
 lazy_static! {
     static ref RUST_LOG: String = env::var("RUST_LOG").unwrap_or_else(|_| String::from("info")); // If not specified, assume logging level is INFO
     static ref RUST_LOG_TYPE: String = env::var("RUST_LOG_TYPE").unwrap_or_else(|_| String::from("console")); // If not specified, assume we're logging to console
 }
 
-pub fn init_logger(file_name: &String) -> String {
+pub fn init_logger(file_name: &str, log_config: Option<&str>) -> String {
+    let config = log4rs::load_config_file(log_config.unwrap_or_default(), Default::default());
+
     /* Logging to file */
     if RUST_LOG_TYPE.to_lowercase().as_str() == "file" {
         log_to_file(file_name, &RUST_LOG);
@@ -22,7 +28,21 @@ pub fn init_logger(file_name: &String) -> String {
 
     /* Logging to console */
     if RUST_LOG_TYPE.to_lowercase().as_str() == "console" {
-        log_to_console(&RUST_LOG);
+        match config {
+            Ok(mut config) => {
+                println!("Use log config in log.yaml");
+                let root = config.root_mut();
+                root.set_level(LevelFilter::from_str(&RUST_LOG).expect("Wrong RUST_LOG level"));
+                log4rs::init_config(config).expect("Cannot init log config");
+            }
+            Err(err) => {
+                println!(
+                    "There is error when loading log.yaml:{:?}, use default log config",
+                    err
+                );
+                log_to_console(&RUST_LOG);
+            }
+        }
         return message(&RUST_LOG_TYPE, &RUST_LOG);
     }
 
