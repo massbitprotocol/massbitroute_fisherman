dd508c361634819169cc293f8d113e5ffd2efd87
anhhuy <anhhuy0501@gmail.com>
2022-09-29 16:46:04 +0700

    update log + deploy script for worker

--- scripts/deploy/run_worker.sh
+++ scripts/deploy/run_worker.sh
@@ -10,5 +10,6 @@ export WORKER_ENDPOINT=http://$WORKER_IP:4040
 export WORKER_SERVICE_ENDPOINT=0.0.0.0:4040
 export BENCHMARK_WRK_PATH=/opt/fisherman/benchmark
 export COMMON_CONFIG_FILE=/opt/fisherman/common.json
+export CONFIG_DIR=/opt/fisherman/
 cd /opt/fisherman
 ./fisherman
--- scripts/deploy/update_bin_and_restart_scheduler.sh
+++ scripts/deploy/update_bin_and_restart_scheduler.sh
@@ -1,13 +1,17 @@
 sudo rm /opt/fisherman -rf
 sudo mkdir /opt/fisherman
+sudo mkdir /opt/fisherman/configs
+
 
 sudo mv ~/scheduler /opt/fisherman/
 sudo mv ~/.env /opt/fisherman/
 sudo mv ~/run.sh /opt/fisherman/
-sudo mv ~/common.json /opt/fisherman/
+sudo mv ~/common.json /opt/fisherman/configs/
+sudo mv ~/log.yaml /opt/fisherman/configs/
+
 chmod +x /opt/fisherman/run.sh
 
-sudo cp ~/configs /opt/fisherman/ -r
-sudo rm -rf ~/configs
+#sudo cp ~/configs /opt/fisherman/ -r
+#sudo rm -rf ~/configs
 
 sudo supervisorctl restart scheduler
