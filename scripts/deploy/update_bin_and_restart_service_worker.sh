1710be5ab0133bba8ea50dadedfd77cf77bb806c
huy <anhhuy0501@gmail.com>
2022-10-15 21:59:35 +0700

    fix deploy script

--- scripts/deploy/update_bin_and_restart_service_worker.sh
+++ scripts/deploy/update_bin_and_restart_service_worker.sh
@@ -1,6 +1,7 @@
 sudo rm /opt/fisherman -rf
 
 sudo cp ~/fisherman /opt/ -r
+sudo mv /opt/fisherman/fisherman.conf /etc/supervisor/conf.d/fisherman.conf
 sudo chmod +x /opt/fisherman/run.sh
 sudo rm -rf ~/fisherman
 
