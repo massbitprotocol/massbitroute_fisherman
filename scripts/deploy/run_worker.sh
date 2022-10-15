7411b2e07792d6687479dfd6ad34efc399807e01
huy <anhhuy0501@gmail.com>
2022-10-15 21:59:35 +0700

    fix deploy script

--- scripts/deploy/run_worker.sh
+++ scripts/deploy/run_worker.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 export DOMAIN=massbitroute.net
-export RUST_LOG=debug
-export SCHEDULER_ENDPOINT=https://scheduler.fisherman.$DOMAIN
+export SCHEDULER_ENDPOINT=http://scheduler.fisherman.$DOMAIN
 export WORKER_IP=$(curl ifconfig.me)
 export ZONE={{ZONE}}
 export WORKER_ID="$ZONE-$WORKER_IP"
 export WORKER_ENDPOINT=http://$WORKER_IP:4040
 export WORKER_SERVICE_ENDPOINT=0.0.0.0:4040
-export BENCHMARK_WRK_PATH=/opt/fisherman/benchmark
-export COMMON_CONFIG_FILE=/opt/fisherman/common.json
-export CONFIG_DIR=/opt/fisherman/
+export SCHEDULER_AUTHORIZATION=11967d5e9addc5416ea9224eee0e91fc
+
 cd /opt/fisherman
 ./fisherman
